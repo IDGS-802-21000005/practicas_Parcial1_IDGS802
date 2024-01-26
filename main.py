d3afabcec4a6023e25efa9dbd129c07979eff092
IDGS-802-21000005 <CarlosIRBello@gmail.com>
2024-01-25 21:17:45 -0600

    CIRB 25/01/24 Operaciones basicas

---
+++ main.py
@@ -0,0 +1,27 @@
+from flask import Flask, request,render_template
+app = Flask(__name__)
+
+@app.route("/",methods=["GET","POST"])
+def calculadora():
+        if request.method=="POST":
+            num1=(request.form.get("n1"))
+            num2=(request.form.get("n2"))
+            op=(request.form.get("operacion"))
+            if "*" in op:
+                res = str(int(num1) * int(num2))
+                return render_template("form.html", resultado=res)
+            if "/" in op:
+                res = str(int(num1) / int(num2))
+                return render_template("form.html", resultado=res)
+            if "-" in op:
+                res = str(int(num1) - int(num2))
+                return render_template("form.html", resultado=res)
+            if "+" in op:
+                res = str(int(num1) + int(num2))
+                return render_template("form.html", resultado=res)
+        else:
+            return render_template("form.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
